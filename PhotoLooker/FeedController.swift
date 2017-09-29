d5a5ff6edc752c96c3bb67af2ae0f4298cd9f5cb
BohdanNikoletti <bohdanrose1@gmail.com>
2017-09-29 20:40:55 +0300

    Starter code added

---
+++ PhotoLooker/FeedController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PhotoLooker
+//
+//  Created by Bohdan Mihiliev on 29.09.17.
+//  Copyright © 2017 Bohdan Mihiliev. All rights reserved.
+//
+
+import UIKit
+
+class FeedController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = UIColor.red
+    }
+}
+

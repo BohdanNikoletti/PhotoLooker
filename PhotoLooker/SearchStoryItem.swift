f328ff7c924743f49d4c02caaaf23c813fba2dd9
BohdanNikoletti <bohdanrose1@gmail.com>
2017-10-01 01:06:14 +0300

    Added history Realm model

--- PhotoLooker/SearchStoryItem.swift
+++ PhotoLooker/SearchStoryItem.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import RealmSwift
+
+class HistoryItem: Object{
+    
+    @objc dynamic var id = 0
+    @objc dynamic var imagePath: String = ""
+    @objc dynamic var requestPhrase: String = ""
+    
+    override static func primaryKey() ->String? {
+        return "id"
+    }
+    
+    static func getHistory() -> [HistoryItem]{
+        let realm = try! Realm()
+        return Array(realm.objects(HistoryItem.self))
+    }
+    
+    static func add(historyItem item: HistoryItem){
+        let realm = try! Realm()
+        do {
+            try realm.write {
+                realm.add(item)
+            }
+        }catch {
+            print("Can not save history item: \(item)")
+        }
+    }
+}
